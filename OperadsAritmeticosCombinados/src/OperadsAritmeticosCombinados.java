19f9214dd397e7ca618b5ce130b76b5e8dd99e6f
Imperdibles <Imperdibles@Imper>
2020-06-10 13:20:34 +0200

    OperadoresAritmeticosCombinados

---
+++ OperadsAritmeticosCombinados/src/OperadsAritmeticosCombinados.java
@@ -0,0 +1,21 @@
+
+public class OperadsAritmeticosCombinados {
+
+	
+	public static void main(String[] args) {
+		int numero = 10;
+		
+		
+		numero += 5; // manera resumida de hacer -> numero = numero + 5; (ahorro código)
+		//numero -= 5; abreviatura resta
+		//numero *= 5; abreviatura multiplicacion
+		//numero /= 5; abreviatura división
+		//numero %= 5; abreviatura resto
+		
+		
+		System.out.println(numero);
+			
+		
+	}
+
+}
